ddec33fa0f800a1393c993444c55ba95a840ddf5
Kai <kb@bellmann-engineering.com>
2024-09-19 10:12:59 +0200

    person inhalt

--- person.py
+++ person.py
@@ -1,7 +1,34 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+from dataclasses import dataclass
+from datetime import datetime
 
-    def greet(self):
-        print(f"Hallo, mein Name ist {self.name} und ich bin {self.age} Jahre alt.")
+@dataclass
+class Office:
+    floor: str
+    location: str
+    max_empl_number : int
+
+class Person:
+    def __init__(self, name, birthdate):
+        self.name = name
+        self.birthdate = datetime.strptime(birthdate, '%d.%m.%Y')
+
+    def __str__(self) -> str:
+        return f"Ich bin {self.name} und bin {self.age} Jahre alt."
+    
+    def __repr__(self) -> str:
+        return f"PERSON: {self.name} ({self.age})"
+    
+    # @property
+    # def age(self) -> int:
+    #     return datetime.now - self.birthdate
+    
+    
+    
+
+p1 = Person("Hans", '1.6.1980')
+#p1.age
+
+o = Office("Mainfloor", "Hauptstr", 24)
+print(o)
+
+
